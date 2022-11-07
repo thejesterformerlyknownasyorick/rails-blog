de867ecc6b8999f6a1e10a70e0a468b0a43a3786
thejesterformerlyknownasyorick <42towels@gmail.com>
2022-11-07 09:43:41 -0800

    Finish view and controller for new/create, with link to #new from #index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
