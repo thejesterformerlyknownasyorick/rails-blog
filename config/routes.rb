b92a50ffb16b1e0281b3b7ec42df9b6cbe0f9526
thejesterformerlyknownasyorick <42towels@gmail.com>
2022-11-07 10:22:44 -0800

    Add comment section, fix routing error where nesting did not include 'do' block

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
